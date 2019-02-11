b2525925440f34094ce9b087126494c13c7c311f
Robin Jain <Robin.Jain1@aexp.com>
2019-02-11 11:04:17 -0700

    fixed bug

--- jobs/app.py
+++ jobs/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 def open_connection():
-    connection = getattr(g,'_connection',False)
+    connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
 
@@ -26,7 +26,7 @@ def execute_sql(sql,values=(),commit=False,single=False):
 
 @app.teardown_appcontext
 def close_connection(exception):
-    connection = getattr(g,"_connection",False)
+    connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
 
